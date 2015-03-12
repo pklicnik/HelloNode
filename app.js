15f427478b969b0d284e0aa23aecb79a43436dc3
Paul Klicnik <pklicnik@ca.ibm.com>
2015-03-12 15:55:16 -0400

    Test ZDD deploy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var server = http.createServer(function (req, res) {
 	res.writeHead(200, {
 		"Content-Type": "text/plain"
 	});
-	res.end("Hello, World!");
+	res.end("Hello, Docker!");
 });
 
 server.listen(port);
