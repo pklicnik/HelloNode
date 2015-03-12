1e717af91f7503f6a8e10d3cd8e9ef6b189c098c
Paul Klicnik <pklicnik@ca.ibm.com>
2015-03-12 16:17:23 -0400

    Script ZDD test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var server = http.createServer(function (req, res) {
 	res.writeHead(200, {
 		"Content-Type": "text/plain"
 	});
-	res.end("Hello, Test!");
+	res.end("Hello, B00m!");
 });
 
 server.listen(port);
