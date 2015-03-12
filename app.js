139fe679a1673a764c1cf0641676e2a7f200fa01
Paul Klicnik <pklicnik@ca.ibm.com>
2015-03-12 16:18:58 -0400

    ZDD

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var server = http.createServer(function (req, res) {
 	res.writeHead(200, {
 		"Content-Type": "text/plain"
 	});
-	res.end("Hello, B00m!");
+	res.end("Hello, there it is!");
 });
 
 server.listen(port);
