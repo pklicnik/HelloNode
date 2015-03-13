7366ac1d8e64e9abf5b5dfde9f1a113748487ea1
Paul K <pklicnik@gmail.com>
2015-03-13 10:02:58 -0400

    Boom roaster

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var server = http.createServer(function (req, res) {
 	res.writeHead(200, {
 		"Content-Type": "text/plain"
 	});
-	res.end("Try something totally different!");
+	res.end("Some ZDD action going on!");
 });
 
 server.listen(port);
